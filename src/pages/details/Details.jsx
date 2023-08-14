b6924d1ffef7f1cc8fc4ab818383280f1da29830
Shikaraman S <83532128+shivkaraman@users.noreply.github.com>
2023-08-14 19:08:50 +0530

    Details page completed

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from 'react';
 
-import './details.css'
+//Hooks
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router';
+
+//Components
+import DetailsBanner from './detaileBanner/DetailsBanner';
+import Cast from './cast/Cast';
+import VideoSection from './videoSection/VideoSection';
+import Recommendation from './carousels/Recommendation';
+import Similar from './carousels/Similar';
+
+//Style
+import './details.css';
 
 const Details = () => {
-  return (
-    <div>Details</div>
-  )
-}
+    const { mediaType, id } = useParams();
+    const { data: video, loading: videoLoading } = useFetch(
+        `/${mediaType}/${id}/videos`
+    ); //Fetch videos related to curr movie
+    const { data: credits, loading: creditsLoading } = useFetch(
+        `/${mediaType}/${id}/credits`
+    ); //Fetch movie team crew, cast members etc
 
-export default Details
+    return (
+        <div>
+            <DetailsBanner
+                video={video?.results[0]} //Passing only trailer
+                crew={credits?.crew} //Passing only crew members
+            />
+            <Cast
+                data={credits?.cast}
+                loading={creditsLoading}
+            />
+            <VideoSection
+                data={video}
+                loading={videoLoading}
+            />
+            <Recommendation
+                mediaType={mediaType}
+                id={id}
+            />
+            <Similar
+                mediaType={mediaType}
+                id={id}
+            />
+        </div>
+    );
+};
+
+export default Details;
